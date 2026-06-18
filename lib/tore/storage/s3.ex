f5c374da0940283a35e0a01be0ff1270c15778fa
Gustaf Rydholm <gustaf@nexure.io>
2026-06-18 19:12:02 +0200

    fix(storage): one-attempt bucket probe so a stopped Garage doesn't spam retries

--- lib/tore/storage/s3.ex
+++ lib/tore/storage/s3.ex
@@ -47,13 +47,21 @@ defmodule Tore.Storage.S3 do
 
   @spec ensure_buckets_exist() :: :ok
   def ensure_buckets_exist do
+    # Single-attempt probe so a stopped Garage doesn't spam 10 retries at boot
+    # or every time an image upload is attempted.
+    opts = [retries: [max_attempts: 1]]
+
     Enum.each(Tore.Storage.Buckets.all(), fn bucket ->
-      case bucket |> ExAws.S3.put_bucket("garage") |> ExAws.request() do
+      case bucket |> ExAws.S3.put_bucket("garage") |> ExAws.request(opts) do
         {:ok, _} ->
           :ok
 
         {:error, {:http_error, 409, _}} ->
           :ok
+
+        {:error, :econnrefused} ->
+          require Logger
+          Logger.info("S3 (Garage) unreachable at boot; skipping bucket probe for #{bucket}")
 
         {:error, reason} ->
           require Logger
